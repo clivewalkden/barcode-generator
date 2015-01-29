08adc118c920c314386162cd7e574bb2ffe9f446
Clive Walkden <clive@sozodesign.co.uk>
2015-01-29 14:22:57 +0000

    Initial commit

---
+++ src/CliveWalkden/Barcodes/BarcodeUPC.php
@@ -0,0 +1,17 @@
+<?php
+class BarcodeUPC {
+
+    static public function getDigit($code){
+        if (strlen($code) < 12) {
+            $code = '0' . $code;
+        }
+        return BarcodeEAN::getDigit($code, 'ean13');
+    }
+
+    static public function compute($code){
+        if (strlen($code) < 12) {
+            $code = '0' . $code;
+        }
+        return substr(BarcodeEAN::compute($code, 'ean13'), 1);
+    }
+}
